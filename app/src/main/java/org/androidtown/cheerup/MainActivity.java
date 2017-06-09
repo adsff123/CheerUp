f45f1ae4c0a342584ff889d062c06751314f3104
Lee_Sang_Hyo <pizsh@naver.com>
2017-06-09 13:18:54 +0900

    first commit

---
+++ app/src/main/java/org/androidtown/cheerup/MainActivity.java
@@ -0,0 +1,13 @@
+package org.androidtown.cheerup;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    //종설프 과제물 깃헙 연결
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
